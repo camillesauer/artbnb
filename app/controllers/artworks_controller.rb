7249628d9bb95ea991612c6469134fd5defc394c
Lucille James <lucille.james00@gmail.com>
2020-05-19 12:34:11 +0200

    added controllers, commented footer for now

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,28 @@
 class ArtworksController < ApplicationController
+
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.save
+    redirect_to artwork_path(@artwork)
+  end
+
+  def edit
+    @artwork = Artwork.find(params[:id])
+  end
+
+  def update
+    @artwork = Artwork.find(params[:id])
+    @artwork.update(artwork_params)
+    redirect_to artwork_path(@artwork)
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:title, :description, :price, :category, :dimension)
+  end
 end
