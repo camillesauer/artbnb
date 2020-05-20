204f090a4f2e832b1e247bfafbba548ff974ab40
Camille Sauer <camille-sauer@hotmail.fr>
2020-05-20 16:08:55 +0200

    fix it

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,5 @@
 class Artwork < ApplicationRecord
+  # before_create :set_default_picture
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
@@ -7,12 +8,13 @@ class Artwork < ApplicationRecord
   has_many :rentals
 
   validates :title, presence: true
-  validates :address, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :category, presence: true
   validates :dimension, presence: true
   CATEGORY = ["Painting", "Sculpture", "Installation", "Photography", "Video"]
   validates :category, inclusion: { in: CATEGORY}
-
+  # def set_default_picture
+  #   self.photo = 'app/assets/images/filler_picture.jpg'
+  # end
 end
