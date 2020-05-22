e95f1bfa702dff29d2dc5e6de47adb502c175d14
Estelle Yapo <estellelaure@yahoo.fr>
2020-05-22 15:35:36 +0200

    view-response

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -8,7 +8,7 @@ class ArtworksController < ApplicationController
       @artworks = Artwork.all
     end
 
-    @artworks_geo = Artwork.geocoded # returns flats with coordinates
+    @artworks_geo = Artwork.geocoded.where("title ILIKE ?", "%#{params[:query]}%") # returns flats with coordinates
     @markers = @artworks_geo.map do |artwork|
       {
         lat: artwork.latitude,
